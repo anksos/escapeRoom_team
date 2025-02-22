b21030b9b3fd3bfd067a9a9e25eebb0401e747d1
Anastasis Xouzafeiris <788969+anksos@users.noreply.github.com>
2025-02-22 21:59:46 +0100

    fixing the logic and moving to rand.NewSource

--- rand_teams.go
+++ rand_teams.go
@@ -7,7 +7,6 @@ import (
 )
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
 	players := []string{
 		"Veronika",
 		"Roberta",
@@ -20,8 +19,10 @@ func main() {
 		"Kyrylo",
 	}
 
-	teamAA, teamAB := createTeams(players)
-	teamBA, teamBB := createTeams(players)
+	rand.NewSource(time.Now().UnixNano())
+	shuffledPlayers := shufflePlayers(players)
+	teamAA, teamAB := createTeams(shuffledPlayers)
+	teamBA, teamBB := createTeams(shufflePlayers(players))
 
 	fmt.Println("Group 1:")
 	fmt.Println("Team AA:", teamAA)
@@ -31,20 +32,17 @@ func main() {
 	fmt.Println("Team BB:", teamBB)
 }
 
+func shufflePlayers(players []string) []string {
+	shuffled := make([]string, len(players))
+	copy(shuffled, players)
+	rand.Shuffle(len(shuffled), func(i, j int) {
+		shuffled[i], shuffled[j] = shuffled[j], shuffled[i]
+	})
+	return shuffled
+}
+
 func createTeams(players []string) ([]string, []string) {
-	players1 := make([]string, len(players))
-	copy(players1, players)
-	players2 := make([]string, len(players))
-	copy(players2, players)
-
-	rand.Shuffle(len(players1), func(i, j int) {
-		players1[i], players1[j] = players1[j], players1[i]
-	})
-	rand.Shuffle(len(players2), func(i, j int) {
-		players2[i], players2[j] = players2[j], players2[i]
-	})
-
-	teamA := players1[:len(players1)/2]
-	teamB := players2[len(players2)/2:]
+	teamA := players[:len(players)/2]
+	teamB := players[len(players)/2:]
 	return teamA, teamB
 }
